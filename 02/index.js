47df917b2f71a3eafd1010ac6b4835af0a27fdf7
Will Harris <will.t.harris@protonmail.com>
2021-12-04 19:50:17 -0800

    format files and move package.json to root

--- 02/index.js
+++ 02/index.js
@@ -7,42 +7,42 @@ let depth = 0;
 let aim = 0;
 
 try {
-  const string = fs.readFileSync("data.txt", "utf8");
+	const string = fs.readFileSync("data.txt", "utf8");
 
-  let stream = new Readable();
-  stream.push(string);
-  stream.push(null); // This indicates the end of the stream.
+	let stream = new Readable();
+	stream.push(string);
+	stream.push(null); // This indicates the end of the stream.
 
-  let readlineStream = readline.createInterface({
-    input: stream,
-    crlfDelay: Infinity,
-  });
+	let readlineStream = readline.createInterface({
+		input: stream,
+		crlfDelay: Infinity,
+	});
 
-readlineStream.on("line", (input) => {
-    const [direction, quantity] = input.split(":");
+	readlineStream.on("line", (input) => {
+		const [direction, quantity] = input.split(":");
 
-    switch (true) {
-      case direction === "forward": {
-        horizontalPosition += Number(quantity);
+		switch (true) {
+			case direction === "forward": {
+				horizontalPosition += Number(quantity);
 
-        depth += aim * Number(quantity);
+				depth += aim * Number(quantity);
 
-        break;
-      }
-      case direction === "up": {
-        aim -= Number(quantity);
-        break;
-      }
-      case direction === "down": {
-        aim += Number(quantity);
-      }
-      default: {
-        break;
-      }
-    }
+				break;
+			}
+			case direction === "up": {
+				aim -= Number(quantity);
+				break;
+			}
+			case direction === "down": {
+				aim += Number(quantity);
+			}
+			default: {
+				break;
+			}
+		}
 
-    console.log("Final value: ", horizontalPosition * depth);
-  });
+		console.log("Final value: ", horizontalPosition * depth);
+	});
 } catch (error) {
-  console.error(error);
+	console.error(error);
 }
--- 03/index.js
+++ 03/index.js
@@ -11,43 +11,43 @@ stream.push(string);
 stream.push(null);
 
 let readlineStream = createInterface({
-  input: stream,
-  crlfDelay: Infinity,
+	input: stream,
+	crlfDelay: Infinity,
 });
 
 // store each line as a string in array
 readlineStream.on("line", (line) => {
-  parsedArray.push(line);
+	parsedArray.push(line);
 });
 
 readlineStream.on("close", () => {
-  let gammaRate = "";
-  let epsilonRate = "";
+	let gammaRate = "";
+	let epsilonRate = "";
 
-  for (let i = 0; i < 12; i++) {
-    let zeroCount = 0;
-    let oneCount = 0;
+	for (let i = 0; i < 12; i++) {
+		let zeroCount = 0;
+		let oneCount = 0;
 
-    for (let j = 0; j < parsedArray.length; j++) {
-      if (parsedArray[j][i] === "0") {
-        zeroCount++;
-      } else {
-        oneCount++;
-      }
-    }
+		for (let j = 0; j < parsedArray.length; j++) {
+			if (parsedArray[j][i] === "0") {
+				zeroCount++;
+			} else {
+				oneCount++;
+			}
+		}
 
-    if (zeroCount > oneCount) {
-      gammaRate = `${gammaRate}0`;
-      epsilonRate = `${epsilonRate}1`;
-    } else {
-      gammaRate = `${gammaRate}1`;
-      epsilonRate = `${epsilonRate}0`;
-    }
-  }
+		if (zeroCount > oneCount) {
+			gammaRate = `${gammaRate}0`;
+			epsilonRate = `${epsilonRate}1`;
+		} else {
+			gammaRate = `${gammaRate}1`;
+			epsilonRate = `${epsilonRate}0`;
+		}
+	}
 
-  const finalGamma = parseInt(gammaRate, 2);
-  const finalEpsilon = parseInt(epsilonRate, 2);
-  const powerConsumption = finalGamma * finalEpsilon;
+	const finalGamma = parseInt(gammaRate, 2);
+	const finalEpsilon = parseInt(epsilonRate, 2);
+	const powerConsumption = finalGamma * finalEpsilon;
 
-  console.log(powerConsumption);
+	console.log(powerConsumption);
 });
